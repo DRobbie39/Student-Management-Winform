a1e584e264a8f0b97cf03c9e348b1df086f2018d
DRobbie39 <phangiadat123@gmail.com>
2025-11-09 17:59:53 +0700

    add main form

--- StudentManagement/StudentManagement/Program.cs
+++ StudentManagement/StudentManagement/Program.cs
@@ -1,3 +1,5 @@
+using StudentManagement.UI.Admin;
+
 namespace StudentManagement
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace StudentManagement
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new frmAdminMainForm());
         }
     }
 }
