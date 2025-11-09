30fa131b0b1a38475310300e2a9e4f381d4497e2
DRobbie39 <phangiadat123@gmail.com>
2025-11-09 18:18:20 +0700

    add database

---
+++ StudentManagement/StudentManagement/DAL/Models/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagement.DAL.Models;
+
+public partial class Score
+{
+    public int ScoreId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public int SubjectId { get; set; }
+
+    public double? Score1 { get; set; }
+
+    public DateOnly? ExamDate { get; set; }
+
+    public virtual Student Student { get; set; } = null!;
+
+    public virtual Subject Subject { get; set; } = null!;
+}
---
+++ StudentManagement/StudentManagement/DAL/Models/StudentManagementContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagement.DAL.Models;
+
+public partial class StudentManagementContext : DbContext
+{
+    public StudentManagementContext()
+    {
+    }
+
+    public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    public virtual DbSet<Class> Classes { get; set; }
+
+    public virtual DbSet<Score> Scores { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    public virtual DbSet<Subject> Subjects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-UTPMHK27\\SQLEXPRESS;Database=StudentManagement;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.AccountId).HasName("PK__Accounts__349DA5862416D0D2");
+
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.CreatedDate).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.PasswordHash).HasMaxLength(255);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.ClassId).HasName("PK__Classes__CB1927A0F2A8A1F4");
+
+            entity.Property(e => e.ClassId).HasColumnName("ClassID");
+            entity.Property(e => e.ClassName).HasMaxLength(50);
+            entity.Property(e => e.Department).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Score>(entity =>
+        {
+            entity.HasKey(e => e.ScoreId).HasName("PK__Scores__7DD229F1FFAB5669");
+
+            entity.Property(e => e.ScoreId).HasColumnName("ScoreID");
+            entity.Property(e => e.Score1).HasColumnName("Score");
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+            entity.Property(e => e.SubjectId).HasColumnName("SubjectID");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Scores)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK_Scores_Students");
+
+            entity.HasOne(d => d.Subject).WithMany(p => p.Scores)
+                .HasForeignKey(d => d.SubjectId)
+                .HasConstraintName("FK_Scores_Subjects");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Students__32C52A794BF906F3");
+
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+            entity.Property(e => e.Address).HasMaxLength(200);
+            entity.Property(e => e.ClassId).HasColumnName("ClassID");
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.Gender).HasMaxLength(10);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Students)
+                .HasForeignKey(d => d.ClassId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK_Students_Classes");
+        });
+
+        modelBuilder.Entity<Subject>(entity =>
+        {
+            entity.HasKey(e => e.SubjectId).HasName("PK__Subjects__AC1BA38839702233");
+
+            entity.Property(e => e.SubjectId).HasColumnName("SubjectID");
+            entity.Property(e => e.SubjectName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
